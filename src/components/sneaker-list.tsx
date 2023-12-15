b6994c4f33fcfa81a04d4fe6d45ab7b532352cb4
flowseph1 <josephthenewandarmonius@gmail.com>
2023-12-15 00:17:22 -0600

    feat: update sneaker item style

--- src/components/sneaker-list.tsx
+++ src/components/sneaker-list.tsx
@@ -3,6 +3,8 @@
 import { IconButton } from "@/components/ui";
 import { products } from "@/data/producst";
 import { Sneaker } from "@/types/sneaker";
+import Image from "next/image";
+import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
 import { IoHeartOutline } from "react-icons/io5";
 
@@ -22,30 +24,33 @@ interface Props {
 
 function SneakerItem({ sneaker }: Props) {
   return (
-    <div className="group relative flex h-[27rem] w-full flex-col overflow-hidden rounded-lg border-[0.1rem] border-border-color hover:shadow-product-shadow">
-      <div className="flex-1 bg-container-extra-light">
+    <Link
+      href={sneaker.slug}
+      className="group relative flex min-h-[27rem] w-full flex-col overflow-hidden rounded-lg border-[0.1rem] border-border-color group:"
+    >
+      <div className="flex-1 bg-container-extra-light group-hover:bg-neutral-900">
         <div className="flex items-center justify-center">
-          <img
+          <Image
             src={sneaker.grid_picture_url}
             alt="Foto principal"
             width={160}
-            className="cursor-pointer transition-transform group-hover:scale-110"
+            height={160}
+            className="transition-transform group-hover:scale-110"
           />
         </div>
       </div>
-      <div className="flex flex-[0.5] flex-col bg-container-light p-5">
-        <div className="mb-2 cursor-pointer text-sm font-bold hover:underline">
-          {sneaker.name}
+      <div className="flex flex-col flex-1 bg-container-light p-5 shrink-0 gap-4">
+        <div className="inline-flex h-fit cursor-pointer truncate rounded-full bg-container/50 px-4 py-1 text-[1rem] w-fit ">
+          {sneaker.brand_name}
         </div>
+
+        <div className="text-sm font-semibold">{sneaker.name}</div>
 
         <div className="flex flex-1 justify-between">
           <div className="flex flex-1 flex-wrap space-x-3">
-            <div className="inline-flex h-fit cursor-pointer truncate rounded-full bg-container px-4 py-1 text-xs">
-              {sneaker.brand_name}
-            </div>
-            {/*  <div className="inline-flex cursor-pointer truncate rounded-full bg-container px-4 py-1 text-xs">
-                            {sneaker.nickname}
-                        </div> */}
+            {/* <div className="inline-flex cursor-pointer truncate rounded-full bg-container px-4 py-1 text-xs">
+              {sneaker.nickname}
+            </div> */}
           </div>
 
           <div className="flex flex-1 items-end justify-end ">
@@ -57,6 +62,6 @@ function SneakerItem({ sneaker }: Props) {
       <div className="absolute right-5 top-5 cursor-pointer text-text-light transition-transform hover:scale-110">
         <IoHeartOutline size={18} />
       </div>
-    </div>
+    </Link>
   );
 }
