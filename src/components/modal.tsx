c57b5b41f47aa7f106f8b72417be6a69baa32b39
flowseph1 <josephthenewandarmonius@gmail.com>
2024-10-08 23:02:35 -0600

    feat: add modal component

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,25 +1,27 @@
 "use client";
 
-import { Button } from "@/components/ui";
-import { cn } from "@/lib/utils";
+import { cn, sleep } from "@/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { memo, useEffect, useRef } from "react";
 import { IoClose } from "react-icons/io5";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@/components/ui/buttons";
 
 interface ModalProps {
 	name: string;
 	children: React.ReactNode;
 	title?: string;
 	buttonTitle?: string;
+	onClose?: () => void;
 	buttonAction?: () => void;
 }
 
-export function Modal({
+function Modal_({
 	title,
 	children,
 	name,
 	buttonTitle,
+	onClose,
 	buttonAction,
 }: ModalProps) {
 	const dialogRef = useRef<HTMLDialogElement>(null);
@@ -35,8 +37,10 @@ export function Modal({
 			dialogRef.current?.showModal();
 		}
 	};
-	const handleClose = () => {
+	const handleClose = async () => {
 		router.replace(pathname);
+		await sleep(400);
+		onClose && onClose();
 		dialogRef.current?.close();
 	};
 
@@ -44,51 +48,64 @@ export function Modal({
 	useEffect(handleToggleModalEffect, [p]);
 
 	return (
-		<motion.dialog
-			ref={dialogRef}
-			onClick={handleClose}
-			className="bg-neutral-600 rounded-xl shadow-xl text-white/90 border-[1px] border-neutral-50"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-		>
-			<div onClick={(e) => e.stopPropagation()} className="">
-				{/* Header */}
-				{title && (
-					<div className="flex justify-between py-5 px-6 border-b-[1px] border-b-neutral-300">
-						<p className="text-lg font-semibold">{title}</p>
-						<button
-							type="button"
-							onClick={handleClose}
-							className="flex justify-center items-center w-10 h-10"
-						>
-							<IoClose />
-						</button>
-					</div>
-				)}
-
-				{/* Body */}
-				<div className="min-w-[300px] py-7 px-6 bg-neutral-900">{children}</div>
-
-				{/* Footer */}
-				<div
-					className={cn(
-						"flex justify-between py-5 px-6 border-t-[1px] border-t-neutral-300",
-						{
-							"!justify-end": !buttonTitle,
-						},
-					)}
+		<AnimatePresence>
+			{p === name && (
+				<motion.dialog
+					ref={dialogRef}
+					onClick={handleClose}
+					className="bg-neutral-600 rounded-xl shadow-xl text-white/90 border-[1px] border-neutral-50 backdrop:bg-black/30"
+					onKeyDown={(e) => {
+						if (e.key === "Escape") {
+							e.preventDefault();
+							handleClose();
+						}
+					}}
+					initial={{ opacity: 0, scale: 0.95 }}
+					animate={{ opacity: 1, scale: 1 }}
+					exit={{ opacity: 0, scale: 0.95 }}
 				>
-					<Button intent="tertiary" title="Cancelar" onClick={handleClose} />
-					{buttonTitle && buttonAction && (
-						<Button
-							intent="primary"
-							title={buttonTitle}
-							onClick={buttonAction}
-						/>
-					)}
-				</div>
-			</div>
-		</motion.dialog>
+					<div onClick={(e) => e.stopPropagation()} className="">
+						{/* Header */}
+						{title && (
+							<div className="flex justify-between py-5 px-6 border-b-[1px] border-b-neutral-400 bg-neutral-900">
+								<p className="text-[1.7rem] font-semibold text-text-base">
+									{title}
+								</p>
+								<button
+									type="button"
+									onClick={handleClose}
+									className="flex justify-center items-center w-10 h-10"
+								>
+									<IoClose />
+								</button>
+							</div>
+						)}
+
+						{/* Body */}
+						<div className="min-w-[300px] py-7 px-6 bg-neutral-900">
+							{children}
+						</div>
+
+						{/* Footer */}
+						<div
+							className={cn(
+								"flex justify-end py-5 px-6 border-t-[1px] border-t-neutral-400 bg-neutral-900/40",
+							)}
+						>
+							{/* <Button intent="ghost" title="Cancelar" onClick={handleClose} /> */}
+							{buttonTitle && buttonAction && (
+								<Button
+									intent="primary"
+									title={buttonTitle}
+									onClick={buttonAction}
+								/>
+							)}
+						</div>
+					</div>
+				</motion.dialog>
+			)}
+		</AnimatePresence>
 	);
 }
+
+export const Modal = memo(Modal_);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
 	return twMerge(clsx(inputs));
 };
+
+export const sleep = (ms: number) => {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+};
