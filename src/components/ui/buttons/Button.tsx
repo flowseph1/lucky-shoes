74090edb14d4fd227fc4f2617404db1afe973c0a
flowseph1 <josephthenewandarmonius@gmail.com>
2024-12-02 22:25:57 -0600

    feat(button): wrap button on Link when href is passed

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -1,120 +1,118 @@
-"use client";
+'use client'
 
-import { Spinner } from "@/components/spinner";
-import { cva, VariantProps } from "class-variance-authority";
-import Link from "next/link";
-import React, { ButtonHTMLAttributes, memo } from "react";
+import { Spinner } from '@/components/spinner'
+import { cva, VariantProps } from 'class-variance-authority'
+import Link from 'next/link'
+import React, { ButtonHTMLAttributes, memo } from 'react'
 
 const button = cva(
-  "inline-flex items-center justify-center button cursor-pointer transition-colors duration-300",
-  {
-    variants: {
-      intent: {
-        primary:
-          "bg-primary-500 font-semibold text-text-base hover:bg-primary-700 border-[0.1rem] border-primary-300",
-        secondary:
-          "bg-primary-100 font-semibold text-primary-900 border-[0.1rem] border-primary-300 hover:bg-primary-200",
-        tertiary:
-          "border-[rgba(255,255,255,.05)] bg-[rgba(255,255,255,.02)] hover:bg-[rgba(255,255,255,.03)] border-[0.1rem] backdrop-blur",
-        ghost:
-          "bg-transparent hover:bg-neutral-500 text-white/70 hover:text-white/90 border-[0.1rem] border-transparent",
-      },
-      size: {
-        small: "h-10 px-5 text-sm",
-        medium: "h-14 px-6 py-3",
-        large: "h-5 px-4 py-2",
-      },
+	'inline-flex items-center justify-center button cursor-pointer transition-colors duration-300',
+	{
+		variants: {
+			intent: {
+				primary:
+					'bg-primary-500 font-semibold text-text-base hover:bg-primary-700 border-[0.1rem] border-primary-300',
+				secondary:
+					'bg-primary-100 font-semibold text-primary-900 border-[0.1rem] border-primary-300 hover:bg-primary-200',
+				tertiary:
+					'border-[rgba(255,255,255,.05)] bg-[rgba(255,255,255,.02)] hover:bg-[rgba(255,255,255,.03)] border-[0.1rem] backdrop-blur',
+				ghost:
+					'bg-transparent hover:bg-neutral-500 text-white/70 hover:text-white/90 border-[0.1rem] border-transparent',
+				danger: 'bg-red-800 font-semibold text-red-100 hover:bg-red-800',
+			},
+			size: {
+				small: 'h-10 px-5 text-sm',
+				medium: 'h-14 px-6 py-3',
+				large: 'h-5 px-4 py-2',
+			},
 
-      rounded: {
-        sm: "rounded-sm",
-        md: "rounded-md",
-        lg: "rounded-lg",
-        xl: "rounded-xl",
-        full: "rounded-full",
-      },
+			rounded: {
+				sm: 'rounded-sm',
+				md: 'rounded-md',
+				lg: 'rounded-lg',
+				xl: 'rounded-xl',
+				full: 'rounded-full',
+			},
 
-      disabled: {
-        true: "cursor-not-allowed opacity-50",
-      },
-    },
+			disabled: {
+				true: 'cursor-not-allowed opacity-50',
+			},
+		},
 
-    defaultVariants: {
-      intent: "primary",
-      size: "medium",
-      rounded: "lg",
-    },
-  },
-);
+		defaultVariants: {
+			intent: 'primary',
+			size: 'medium',
+			rounded: 'lg',
+		},
+	},
+)
 
 interface BaseProps
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof button> {
-  title: string;
-  leftIcon?: React.ReactNode;
-  disabled?: boolean;
-  isLoading?: boolean;
+	extends ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof button> {
+	title: string
+	leftIcon?: React.ReactNode
+	disabled?: boolean
+	isLoading?: boolean
 }
 
 interface LinkType extends BaseProps {
-  href: string;
-  onClick?: never;
+	href: string
 }
 
-interface DefaultType extends BaseProps {
-  onClick: (() => void) | undefined;
-  href?: never;
-}
+interface DefaultType extends BaseProps {}
 
-export type ButtonProps = LinkType | DefaultType;
+export type ButtonProps = LinkType | DefaultType
 
 const Button = (props: ButtonProps) => {
-  if ("onClick" in props) {
-    return (
-      <button
-        disabled={props.disabled}
-        type={props.type ?? "button"}
-        className={button({
-          intent: props.intent,
-          size: props.size,
-          className: props.className,
-          disabled: props.disabled,
-          rounded: props.rounded,
-        })}
-        onClick={props.onClick}
-      >
-        {props.isLoading ? (
-          <Spinner size="16" />
-        ) : (
-          <>
-            {props.leftIcon && <div className="mr-2">{props.leftIcon}</div>}
-            <p>{props.title}</p>
-          </>
-        )}
-      </button>
-    );
-  } else {
-    return (
-      <Link
-        className={button({
-          intent: props.intent,
-          size: props.size,
-          className: props.className,
-          disabled: props.disabled,
-          rounded: props.rounded,
-        })}
-        href={props.href}
-      >
-        {props.isLoading ? (
-          <p>isLoading...</p>
-        ) : (
-          <>
-            {props.leftIcon && <div className="mr-2">{props.leftIcon}</div>}
-            <p>{props.title}</p>
-          </>
-        )}
-      </Link>
-    );
-  }
-};
+	if ('href' in props) {
+		return (
+			<Link
+				className={button({
+					intent: props.intent,
+					size: props.size,
+					className: props.className,
+					disabled: props.disabled,
+					rounded: props.rounded,
+				})}
+				href={props.href}
+			>
+				{props.isLoading ? (
+					<p>isLoading...</p>
+				) : (
+					<>
+						{props.leftIcon && <div className="mr-2">{props.leftIcon}</div>}
+						<p>{props.title}</p>
+					</>
+				)}
+			</Link>
+		)
+	} else {
+		return (
+			<button
+				disabled={props.disabled}
+				type={props.type ?? 'button'}
+				className={button({
+					intent: props.intent,
+					size: props.size,
+					className: props.className,
+					disabled: props.disabled,
+					rounded: props.rounded,
+				})}
+				onClick={props.onClick}
+				{...props}
+			>
+				{props.isLoading ? (
+					<Spinner size="16" />
+				) : (
+					<>
+						{props.leftIcon && <div className="mr-2">{props.leftIcon}</div>}
+						<p>{props.title}</p>
+					</>
+				)}
+			</button>
+		)
+	}
+}
 
-export default memo(Button);
+export default memo(Button)
