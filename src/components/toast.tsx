a2cb9dd6a10d72e4f0a4119a20e7aade38dda9f0
flowseph1 <josephthenewandarmonius@gmail.com>
2024-12-02 22:26:29 -0600

    feat(toast): add missing error toast style

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -1,19 +1,20 @@
 'use client'
 
 import { Toast as ToastProps, useToastStore } from '@/lib/store/toast'
-import { FaCheckCircle } from 'react-icons/fa'
+import { cn } from '@/lib/utils'
+import { FaCheckCircle, FaExclamationCircle } from 'react-icons/fa'
 import { IoClose } from 'react-icons/io5'
 
 const ICONS = {
 	success: <FaCheckCircle className="text-green-200 w-7 h-7" />,
-	error: <FaCheckCircle className="text-red-200 w-7 h-7" />,
+	error: <FaExclamationCircle className="text-red-200 w-7 h-7" />,
 }
 
 export function Toast() {
 	const toasts = useToastStore((state) => state.toasts)
 
 	return (
-		<div className="absolute top-0 right-0 z-50 flex flex-col gap-4 p-4">
+		<div className="absolute top-5 right-5 z-50 flex flex-col gap-4 p-4">
 			{toasts.map((toast) => (
 				<ToastItem key={toast.id} {...toast} />
 			))}
@@ -25,15 +26,21 @@ export function ToastItem(props: ToastProps) {
 	const removeToast = useToastStore((state) => state.removeToast)
 
 	return (
-		<div className="flex gap-3 p-4 bg-green-950 text-green-200 rounded-xl items-center">
+		<div
+			className={cn(
+				'flex gap-3 p-4 bg-green-950 text-green-200 rounded-xl items-center',
+				{
+					'bg-red-950 text-red-200': props.type === 'error',
+				},
+			)}
+		>
 			<div>{ICONS[props.type]}</div>
-			<div>
-				{props.title}
-				{props.description}
-			</div>
+			<div>{props.message}</div>
 			<button
 				type="button"
-				className="text-green-200 hover:text-green-300 transition-colors"
+				className={cn('text-green-200 hover:text-green-300 transition-colors', {
+					'text-red-200 hover:text-red-300 bg-red-950': props.type === 'error',
+				})}
 				onClick={() => removeToast(props.id)}
 			>
 				<IoClose />
