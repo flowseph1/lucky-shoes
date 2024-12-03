0a196b69bb17761890275924ba65c7c18770e445
flowseph1 <josephthenewandarmonius@gmail.com>
2024-12-02 22:30:00 -0600

    refactor(db): correct code based on drizzle docs

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -5,12 +5,6 @@ import postgres from 'postgres'
 
 config({ path: '.env.local' }) // Load environment variables
 
-// Global variable to hold the client
-let client: ReturnType<typeof postgres>
+const client = postgres(process.env.DATABASE_URL!, { prepare: false }) // Adjust max connections if needed
 
-if (!global.dbClient) {
-	client = postgres(process.env.DATABASE_URL!, { max: 10 }) // Adjust max connections if needed
-	global.dbClient = drizzle(client) // Assign to a global variable
-}
-
-export const db = global.dbClient
+export const db = drizzle(client)
