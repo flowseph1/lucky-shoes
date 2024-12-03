eca4c046469af85834bba621ad9798b16fa7a9f9
flowseph1 <josephthenewandarmonius@gmail.com>
2024-12-02 22:30:34 -0600

    feat(table-actions): add table action component

--- src/components/table-action.tsx
+++ src/components/table-action.tsx
@@ -1,13 +1,37 @@
-export function TableAction({
-	children,
-	onClick,
-}: { children: React.ReactNode; onClick: (...any: any) => void }) {
+import Link from 'next/link'
+
+export type TableActionProps = {
+	children: React.ReactNode
+} & (
+	| {
+			href: string
+			onClick?: (...any: any) => void
+	  }
+	| {
+			onClick: (...any: any) => void
+	  }
+)
+
+export function TableAction(props: TableActionProps) {
+	if ('href' in props) {
+		return (
+			<Link href={props.href}>
+				<button
+					onClick={props.onClick}
+					className="flex relative w-14 h-14 justify-center items-center rounded-full hover:bg-neutral-500 cursor-pointer"
+				>
+					{props.children}
+				</button>
+			</Link>
+		)
+	}
+
 	return (
 		<button
-			onClick={onClick}
+			onClick={props.onClick}
 			className="flex relative w-14 h-14 justify-center items-center rounded-full hover:bg-neutral-500 cursor-pointer"
 		>
-			{children}
+			{props.children}
 		</button>
 	)
 }
